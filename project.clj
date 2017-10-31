cb979ac993824848859171e3f67ae3e0cfb0cd24
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-10-30 21:38:23 -0400

    Configure release build

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject culture-map "0.0.1"
   :dependencies [; server
-                 [org.clojure/clojure "1.9.0-beta1"]
+                 [org.clojure/clojure "1.9.0-alpha19"]
                  [http-kit "2.2.0"]
                  [javax.servlet/servlet-api "2.5"]
                  [compojure "1.6.0"]
@@ -8,13 +8,14 @@
                  [hiccup "1.0.5"]
 
                  ; client
-                 [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/clojurescript "1.9.854"]
                  [garden "1.3.2"]
-                 [reagent "0.8.0-alpha1"]
+                 [reagent "0.8.0-alpha2"]
                  [re-frame "0.10.1"]
                  [cljs-ajax "0.7.2"]]
 
-  :plugins [[lein-figwheel "0.5.13"]]
+  :plugins [[lein-figwheel "0.5.13"]
+            [lein-cljsbuild "1.1.7" :exclude [org.clojure/clojure]]]
 
   :main culture-map.server.core
 
@@ -24,9 +25,24 @@
   :figwheel {:server-port 6592}
 
   :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src"]
+                        :source-paths ["src/culture_map/client"]
                         :figwheel {:on-jsload "culture-map.client.core/reload"}
                         :compiler {:main "culture-map.client.core"
                                    :asset-path "/js/out"
                                    :output-to "resources/public/js/culture-map.js"
-                                   :output-dir "resources/public/js/out"}}]})
+                                   :output-dir "resources/public/js/out/dev"}}
+
+                       {:id "release"
+                        :source-paths ["src/culture_map/client"]
+                        :compiler {:main "culture-map.client.core"
+                                   :asset-path "/js/out"
+                                   :output-to "resources/public/js/culture-map.js"
+                                   :output-dir "resources/public/js/out/prod"
+                                   :optimizations :advanced}}]}
+                                   ; to debug advanced compilation issues, enable these options:
+                                   ;:source-map "resources/public/js/culture-map.js.map"
+                                   ;:pseudo-names true
+                                   ;:pretty-print true}}]}
+
+  :profiles {:uberjar {:aot :all
+                       :prep-tasks [["cljsbuild" "once" "release"]]}})
