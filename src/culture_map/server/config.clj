d88c4c75be687149368ce401f59e1d8349729d3d
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-03-03 20:32:05 +0800

    Prevent error when config.edn is missing

--- src/culture_map/server/config.clj
+++ src/culture_map/server/config.clj
@@ -1,8 +1,15 @@
 (ns culture-map.server.config
   (:refer-clojure :exclude [get]))
 
+(defn file
+  "Returns file object, or nil if does not exist"
+  [path]
+  (let [f (clojure.java.io/file path)]
+    (if (.exists f) f nil)))
+
 (defn get [key]
   (some-> "config.edn"
+          file
           slurp
           read-string
           key))
