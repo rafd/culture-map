3f990e6aef32e9774d70dfcc17ce46a6c9882233
Canna Wen <cannawen@gmail.com>
2018-04-18 12:11:02 +0800

    Extend config to read from system env or file

--- src/culture_map/server/config.clj
+++ src/culture_map/server/config.clj
@@ -7,9 +7,18 @@
   (let [f (clojure.java.io/file path)]
     (if (.exists f) f nil)))
 
-(defn get [key]
+(defn- get-from-file [key]
   (some-> "config.edn"
           file
           slurp
           read-string
           key))
+
+(defn- get-from-env [key]
+  (some-> (System/getenv "CONFIG")
+          read-string
+          key))
+
+(defn get [key]
+  (or (get-from-file key)
+      (get-from-env key)))
