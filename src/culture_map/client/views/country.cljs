581a4afb00b5694092de8fe242fb8d7456302d97
Canna Wen <cannawen@gmail.com>
2018-05-14 12:31:17 +0300

    Refactor to split editing and viewing view states [#24]

--- src/culture_map/client/views/country.cljs
+++ src/culture_map/client/views/country.cljs
@@ -16,39 +16,42 @@
 (defn variant-selected? [variant custom country-id]
   (contains? (set (selected-variants custom country-id)) variant))
 
-(defn country-view [country-id editing?]
+(defn view-country-view [country-id]
   (when-let [country @(subscribe [:country country-id])]
     [:div
      [:h1 (country :country/name)]
-
-     (if editing?
-       [:button
-        {:on-click (fn [_] (dispatch [:save-country! country-id]))}
-        "Done"]
-       [:a
-        {:href (routes/edit-country-path {:id country-id})}
-        "Edit"])
-     (when editing?
-       [:button {:on-click
-                 (fn [_]
-                   (dispatch [:country-new-custom! country-id]))}
-        "New custom"])
-     (for [custom (if editing?
-                    @(subscribe [:customs+])
-                    @(subscribe [:country-customs country-id]))]
+     [:a
+      {:href (routes/edit-country-path {:id country-id})}
+      "Edit"]
+     (for [custom @(subscribe [:country-customs country-id])]
        [:div {:key (custom :custom/id)}
         [:h2 (custom :custom/name)]
-        (if editing?
-          (for [variant (custom :custom/variants)]
-            [:span {:key (str (variant :variant/id) country-id)}
-             [:input {:type "checkbox"
-                      :value (variant :variant/name)
-                      :checked (when (variant-selected? variant custom country-id) "checked")}]
-             (variant :variant/name)])
-          (for [variant (selected-variants custom country-id)]
-            [:h3 {:key (variant :variant/id)}
-             (variant :variant/name)]))])]))
+        (for [variant (selected-variants custom country-id)]
+          [:h3 {:key (variant :variant/id)}
+           (variant :variant/name)])])]))
 
 
+(defn edit-country-view [country-id]
+  (when-let [country @(subscribe [:country country-id])]
+    [:div
+     [:h1 (country :country/name)]
+     [:button
+      {:on-click (fn [_] (dispatch [:save-country! country-id]))}
+      "Done"]
+     [:button {:on-click
+               (fn [_]
+                 (dispatch [:country-new-custom! country-id]))}
+      "New custom"]
+     (for [custom @(subscribe [:customs+])]
+       [:div {:key (custom :custom/id)}
+        [:h2 (custom :custom/name)]
+        (for [variant (custom :custom/variants)]
+          [:span {:key (str (variant :variant/id) country-id)}
+           [:input {:type "checkbox"
+                    :checked (when (variant-selected? variant custom country-id) "checked")}]
+           (variant :variant/name)])])]))
 
-
+(defn country-view [country-id editing?]
+  (if editing?
+    (edit-country-view country-id)
+    (view-country-view country-id)))
